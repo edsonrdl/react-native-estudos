4e306f243a963f10feac7a949db8a5139b162ec6
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2024-05-19 19:22:36 -0300

    feat:alterado

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { ScrollViewComponent, StyleSheet, Text, View } from 'react-native';
-import Component1 from './components/component1';
-import Box from './components/box.js';
-import Style from './style/style.js'
+import { StatusBar } from "expo-status-bar";
+import { ScrollViewComponent, StyleSheet, Text, View } from "react-native";
+import Component1 from "./components/component1";
+import Box from "./components/box.js";
+import Style from "./style/style.js";
 
 export default function App() {
-  let vExibir=true
+  let vExibir = true;
   return (
-  <View >
-      <View style={{flexDirection:'column',height:"100%",justifyContent:'center',alignItems:'center'}}>
-     <Box/>
-    <View  style={Style.Text}>
-    <Component1/>
+    <View>
+      <View
+        style={{
+          flexDirection: "column",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box />
+        <View style={Style.Text}>
+          <Component1 />
+        </View>
+      </View>
     </View>
-    </View>
-  </View>
   );
-
-
-};
+}
